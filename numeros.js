e0cfb64696e7c1bb4259d9f26d6a39481094b70b
Vinícius Ribeiro <107771171+Vjfrib@users.noreply.github.com>
2023-04-22 05:36:59 +0000

    upload da primeira versão implemntada

--- numeros.js
+++ numeros.js
@@ -1,30 +1,23 @@
+
+
 let LE = 1;
 let LD = 100;
-let meio;
-let cont = 0;
+let meio = parseInt((LE + LD)/2) //converte valor
 
-window.onload = function() {
-  let mensagem = "Pense em um número entre 1 e 100. É menor que 50?";
-  document.getElementById("mensagem").innerHTML = mensagem;
-  jogar();
-};
+console.log(meio)
 
-function jogar() {
-  meio = parseInt((LE + LD) / 2);
-  cont++;
-  let resposta = prompt("O número que você pensou é <, = ou > " + meio + "?");
-  
-  if (resposta === '=') {
-    let mensagem = "Acertei! O número era " + meio + ". Foram necessárias " + cont + " tentativas.";
-    document.getElementById("mensagem").innerHTML = mensagem;
-  } else if (resposta === '>') {
-    LE = meio + 1;
-    jogar();
-  } else if (resposta === '<') {
-    LD = meio - 1;
-    jogar();
-  } else {
-    alert("Resposta inválida! Tente novamente.");
-    jogar();
-  }
+while (true)
+{
+    
+    let resposta = prompt("o numero que voce pensou eh < = ou >"+meio);
+    meio = parseInt((LE + LD)/2)
+    if (resposta === "="){
+        alert("acertei!")
+        break;
+    }
+    if ( resposta === "<") {
+        LD = (meio-1)
+    } else {
+        LE = (LD+1)
+    }
 }
